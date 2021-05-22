b227c0e9ce4b8f62ae82dbca16fa92771e64f9f5
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2021-05-22 12:34:33 -0700

    Implement 4 algorithms in the paper

--- coalesce/butterfly.py
+++ coalesce/butterfly.py
@@ -3,6 +3,7 @@ import secrets
 from dataclasses import dataclass
 from Cryptodome.Cipher import AES
 from Cryptodome.PublicKey import ECC
+from . import ECIES
 
 
 # Recommended Elliptic Curve Domain Parameters
@@ -13,29 +14,32 @@ SECP256R1_GY = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f
 SECP256R1_N = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
 GEN_P256 = ECC.EccPoint(x=SECP256R1_GX, y=SECP256R1_GY, curve=ECC_CURVE)
 
+AES_KEY_BYTES = 16
+AES_KEY_BITS = 1 << AES_KEY_BYTES
+
 
 def randint(inclusive_lower_bound: int, exclusive_upper_bound: int) -> int:
     return (inclusive_lower_bound +
             secrets.randbelow(exclusive_upper_bound - inclusive_lower_bound))
 
 
-def f_k_int_x(k: bytes, x: int) -> bytes:
+def f_k_int_x(k: bytes, x: int) -> int:
     r"""
     f_k^{int}(x) = (AES(k, x+1) XOR (x+1)) || (AES(k, x+2) XOR (x+2)) || (AES(k, x+3) XOR (x+3))
 
     :param k: the AES key (128-bit).
     :param x: the input block (128-bit).
-    :return: the big-endian integer representation of f_k^{int}(x)
+    :return: the integer of f_k^{int}(x) % SECP256R1_N
     """
+    assert len(k) == AES_KEY_BYTES
     aes_obj = AES.new(k, AES.MODE_ECB)
-    ret = [b'', b'', b'']
+    ret = [0, 0, 0]
     for i in range(1, 4):
-        xpi = (x + i).to_bytes(16, 'big')
+        xpi = (x + i).to_bytes(AES_KEY_BYTES, 'big')
         aes_xpi = aes_obj.encrypt(xpi)
-        blki_int = int.from_bytes(xpi, 'big') ^ int.from_bytes(aes_xpi, 'big')
-        ret[i-1] = blki_int.to_bytes(16, 'big')
+        ret[i-1] = int.from_bytes(xpi, 'big') ^ int.from_bytes(aes_xpi, 'big')
 
-    return b''.join(ret)
+    return ((ret[0] << AES_KEY_BITS << AES_KEY_BITS) | (ret[1] << AES_KEY_BITS) | ret[2]) % SECP256R1_N
 
 
 # def expand_key(i: int,
@@ -71,53 +75,94 @@ def f_k_int_x(k: bytes, x: int) -> bytes:
 #     return prv, pub
 
 def f_x(p0: int, exp: bytes, i: int):
-    # TODO limit i to 64 bit, otherwise it will screw up things
+    # Limit i to 64 bits
+    j = i & ((1 << 64) - 1)
     x = (p0 << 96) | (j << 32) | 0
-    f_k_x = int.from_bytes(f_k_int_x(exp, x), 'big') % SECP256R1_N
+    return f_k_int_x(exp, x)
 
 
 def f_1(exp: bytes, i: int):
     # x is the input to the expansion function
     # 0^{32} || i || j || 0^{32}  for certificate
     # 1^{32} || i || j || 0^{32}  for encryption key
-    return f_x(p0=0, exp, i)
+    return f_x(0, exp, i)
 
 
 def f_2(exp: bytes, i: int):
     # x is the input to the expansion function
     # 0^{32} || i || j || 0^{32}  for certificate
     # 1^{32} || i || j || 0^{32}  for encryption key
-    return f_x(p0=(1 << 32) - 1, exp, i)
+    return f_x((1 << 32) - 1, exp, i)
 
 
-def f_name():
+def f_name(key_id: bytes, i: int) -> bytes:
     # need to figure out NDN name derivation function
     pass
 
+
+@dataclass
+class ButterflyKey:
+    key_id: bytes
+    prv_key: ECC.EccKey
+
+
+# XM: Do we really need such a class?
+@dataclass
+class ButterflyCert:
+    key_id: bytes
+    pub_key: ECC.EccKey
+
+
+# AA: I think I want separate caterpillar public and private keys into two classes
+#     private key could be a derivative class of public
+# XM: The private cocoon is only used by the device to decrypt the certificate.
+#     If we strictly follow the paper, we can use caterpillar+i instead. (Algorithm 4)
+#     So every cocoon is public.
+
+@dataclass
+class Cocoon:
+    """Class for cocoon keys"""
+    key_id: bytes  # Derived name f_name(key_id, i)
+    Bi: ECC.EccPoint
+    Qi: ECC.EccPoint
+
+    def hatch(self) -> typing.Tuple[bytes, ButterflyCert]:
+        # Algorithm 3: CertCoalesce generation
+        # XM: Need to sign the certificate, but this involves the application layer semantics
+        #     Specifically the application namespace
+        c = ECC.generate(curve=ECC_CURVE)
+        a_i_c = ECC.EccKey(point=self.Bi+c.public_key().pointQ, curve=ECC_CURVE)
+        c_bytes = c.export_key(format='DER', use_pkcs8=False)
+        C = ECIES.encrypt(ECC.EccKey(point=self.Qi, curve=ECC_CURVE), c_bytes)
+        return C, ButterflyCert(key_id=self.key_id, pub_key=a_i_c)
+
+
 ## AA: is there way to remove hard-coding of XXXX in to_bytes(XXXX, ..) below. Looks error prone...
+# XM: Switched to secrets.token_bytes(16)
 
 @dataclass
 class Caterpillar:
     """Class for caterpillar keys"""
-    keyId: Name
-    a: int = 0
+    key_id: bytes  # XM: Name Component is bytes; a full name will involve application-layer semantics
+    a: int
     A: ECC.EccPoint
-    p: int = 0
+    p: int
     P: ECC.EccPoint
-    ck: bytes = b''
-    ek: bytes = b''
+    ck: bytes
+    ek: bytes
 
     @staticmethod
     def generate():
-        keyId = None # generate keyId name
+        # Algorithm 1: Generate Caterpillar Key
+        key_id = None  # generate keyId name
         a = randint(1, SECP256R1_N - 1)
         A = GEN_P256 * a
         p = randint(1, SECP256R1_N - 1)
-        P = GEN_P256 * P
-        ck = secrets.randbits(128).to_bytes(16, 'big')
-        ek = secrets.randbits(128).to_bytes(16, 'big')
+        P = GEN_P256 * p
+        ck = secrets.token_bytes(AES_KEY_BYTES)
+        ek = secrets.token_bytes(AES_KEY_BYTES)
 
-        return Caterpillar(keyId=keyId, a=a, A=A, p=p, P=P, ck=ck, ek=ek)
+        return Caterpillar(key_id=key_id, a=a, A=A, p=p, P=P, ck=ck, ek=ek)
 
     def encode_private(self) -> bytes:
         # XM: Should we use PKCS?
@@ -129,15 +174,15 @@ class Caterpillar:
 
     @staticmethod
     def decode_private(extern_key: bytes):
-        keyId = None # extract keyId name
+        key_id = None # extract keyId name
         a = int.from_bytes(extern_key[0:32], 'big')
         A = GEN_P256 * a
         p = int.from_bytes(extern_key[32:64], 'big')
-        P = GEN_P256 * P
+        P = GEN_P256 * p
         ek = extern_key[80:96]
         ck = extern_key[64:80]
 
-        return Caterpillar(keyId=keyId, a=a, A=A, p=p, P=P, ck=ck, ek=ek)
+        return Caterpillar(key_id=key_id, a=a, A=A, p=p, P=P, ck=ck, ek=ek)
 
     def encode_public(self) -> bytes:
         # TODO deal with keyId
@@ -157,58 +202,37 @@ class Caterpillar:
         ek = extern_key[144:160]
 
         # TODO deal with keyId
-        return Caterpillar(a=0, A=A, p=0, P=P, ck=ck, ek=ek)
+        return Caterpillar(key_id=None, a=0, A=A, p=0, P=P, ck=ck, ek=ek)
 
     def to_public(self):
         # TODO deal with keyId
-        return Caterpillar(a=0, A=self.A, p=0, P=self.P, ck=self.ck, ek=self.ek)
+        return Caterpillar(key_id=self.key_id, a=0, A=self.A, p=0, P=self.P, ck=self.ck, ek=self.ek)
+
+    def cocoon(self, i: int) -> Cocoon:
+        # Algorithm 2: Generate Cocoon Keys from public Caterpillar
+        if i < 0 or i >= (1 << 64):
+            raise ValueError(f'Input {i=} should be a 64-bit integer')
+        return Cocoon(key_id=f_name(self.key_id, i),
+                      Bi=self.A + GEN_P256 * f_1(self.ck, i),
+                      Qi=self.P + GEN_P256 * f_2(self.ek, i))
+
+    def butterfly_prv(self, C: bytes, i: int) -> ButterflyKey:
+        # Algorithm 4: Derive Butterfly Private Keys from encrypted C
+        if i < 0 or i >= (1 << 64):
+            raise ValueError(f'Input {i=} should be a 64-bit integer')
+        qi = (self.p + f_2(self.ek, i)) % SECP256R1_N
+        c_bytes = ECIES.decrypt(ECC.EccKey(d=qi, curve=ECC_CURVE), C)
+        c = int(ECC.import_key(c_bytes).d)
+        bi = (self.a + f_1(self.ck, i)) % SECP256R1_N
+        bf_prv_key = (bi + c) % SECP256R1_N
+        return ButterflyKey(key_id=f_name(self.key_id, i),
+                            prv_key=ECC.EccKey(d=bf_prv_key, curve=ECC_CURVE))
 
 
-
-
-
-
-
-    # def sign_key(self) -> str:
+    # def sign_key(self) -> bytes:
     #     prv_key = ECC.EccKey(d=self.a, curve=ECC_CURVE)
     #     return prv_key.export_key(format='DER', use_pkcs8=False)
 
-
-# AA: I think I want separate caterpillar public and private keys into two classes
-#     private key could be a derivative class of public
-
-@dataclass
-class PublicCocoon:
-    """Class for cocoon keys"""
-    Bi: ECC.EccPoint
-    Qi: ECC.EccPoint
-
-    # a_exp_pub:
-    # h_exp_pub: ECC.EccPoint
-    # a_exp_prv: int = 0
-    # h_exp_prv: int = 0
-
-    @staticmethod
-    def hatch(Cat: Caterpillar, i: int = 0):
-        # TBD, need to figure out what this `i` will represent
-
-        Bi = Cat.A + f_1(Cat.ck, i) * GEN_P256 #????  SECP256R1_N  # no clue how to write this
-        Qi = Cat.P + f_2(Cat.ek, i) * GEN_P256 #???? need fix
-
-        return PublicCocoon(Bi=Bi, Qi=Qi)
-        # Qi
-
-@dataclass
-class PrivateCocoon(PublicCocoon):
-    # ???
-    pass
-
-
-class ButterflyCert:
-    pass
-
-class ButterflyKey:
-    pass
     
     # def derive_cocoon(self, i: int = 0, j: int = 0) -> Cocoon:
     #     if i == 0:
